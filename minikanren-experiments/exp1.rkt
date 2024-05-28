3a589a4649a22962bd54fc084adce206d3190658
Edgar Honing <edgar.honing@thinkahead.com>
2024-05-28 10:28:45 -0600

    add appendo

--- minikanren-experiments/exp1.rkt
+++ minikanren-experiments/exp1.rkt
@@ -2,8 +2,28 @@
 
 (require minikanren)
 
-(define (≡ a b) (== a b))
-
 (run* (q) (== 1 1))
 
-(run* (q) (≡ q 42))
+;; functional list append:
+(define (append a b)
+  (cond
+    [(null? a) b]
+    [else (cons (car a) (append (cdr a) b))]))
+
+;; relational list append:
+(define (appendo a b c)
+  (conde
+   ((== '() a) (== b c))
+   ((fresh (d e res)
+           (== `(,d . ,e) a)
+           (== `(,d . ,res) c)
+           (appendo e b res)))))
+
+;; examples using relational append:
+(run* (c) (appendo '(1 2 3) '(4 5 6) c))
+
+(run* (c) (appendo '(1 2 3) c '(1 2 3 4)))
+
+(run* (q) (fresh (a b)
+                 (appendo a b '(1 2 3 4))
+                 (== q (cons a `(,b)))))
