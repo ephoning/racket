96abd6e3eb80f93236767fd423105e8ba0a2357c
e153245@constellation.com <Edgar.Honing@constellation.com>
2026-06-30 17:33:45 -0700

    some provided pict3d examples

---
+++ nuclear-sausage-plant.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+(require racket/gui
+         pict3d
+         pict3d/universe
+         plot/utils)
+
+(current-material (material #:diffuse 0.1 #:specular 0.9 #:roughness 0.1))
+
+(define (make-segment n)
+  (define size (exact->inexact (/ 3 (+ 3 n))))
+  (combine (scale (ellipsoid (pos -1/2 -1/2 0) (pos 1/2 1/2 1))
+                  (dir (* 1/2 size) (* 1/2 size) size))
+           (basis 'bottom (point-at (pos 0 0 0) (dir 0 0 1)))
+           (if (zero? (modulo n 3))
+               (combine
+                (basis 'top (point-at (pos 0 0 size) (dir 1 0 1) #:angle 45))
+                (basis 'top (point-at (pos 0 0 size) (dir -1 0 1) #:angle -45)))
+               (basis 'top (point-at (pos 0 0 size)
+                                     (dir (if (zero? (modulo n 2)) -1 1) 0 1/2)
+                                     #:angle 0)))))
+
+(define (segment-chain n)
+  (cond [(= n 0)  (set-emitted (make-segment n) (emitted "green" 0.25))]
+        [else  (pin* (segment-chain (- n 1)) (make-list n 'top)
+                     (set-emitted (make-segment n) (emitted "green" 0.25))
+                     '(bottom))]))
+
+(define chain (segment-chain 12))
+
+(define (wiggle p)
+  (replace-in-group p '(top) (λ (p) (rotate-z (wiggle p)
+                                              (* 0.5 (+ 0.9 (* 0.1 (random))))))))
+
+(big-bang3d
+ (list chain 0.0)
+ #:name "Nuclear Sausage Plant"
+ #:width 800
+ #:height 600
+ #:on-frame
+ (λ (s n t)
+   (match-define (list p t0) s)
+   ;(printf "frame num: ~a time ~a~n" n (- t t0))
+   (list (rotate-z (wiggle p) 0.5) t))
+ #:on-draw
+ (λ (s n t)
+   (match-define (list p t) s)
+   (combine (basis 'camera (point-at (pos 0 2 4) (dir 0 -1/2 -1)))
+            (sunlight (dir -1 0 -1) (emitted "yellow" 2))
+            p))
+ )
---
+++ spheres-on-canvas.rkt
@@ -0,0 +1,85 @@
+#lang racket
+
+(require racket/gui
+         racket/class
+         pict3d
+         math/flonum
+         profile)
+
+(current-material (material #:ambient 0.05 #:diffuse 0.70 #:specular 0.25 #:roughness 0.1))
+
+(define (random-color)
+  (list (+ (* (random) 0.5) 0.5)
+        (+ (* (random) 0.5) 0.5)
+        (+ (* (random) 0.5) 0.5)))
+
+(define (normalize-color c)
+  (match-define (list r g b) c)
+  (define mx (max r g b))
+  (list (/ r mx) (/ g mx) (/ b mx)))
+
+(define (random-position)
+  (pos (* (- (random) 0.5) 50)
+       (* (- (random) 0.5) 50)
+       (* (- (random) 0.5) 50)))
+
+(define sphere-count 1000)
+
+(define spheres
+  (time
+   (combine
+    (for/list ([_  (in-range sphere-count)])
+      (with-color (rgba (random-color) (if (< (random) 0.5) 0.75 1.0))
+        (sphere (random-position)
+                (* 0.25 (+ (random) 0.1)))))
+    (for/list ([_  (in-range 500)])
+      (let* ([rgb  (normalize-color (random-color))]
+             [pos  (random-position)]
+             [int  (+ 0.25 (* (random) 0.25))])
+        (combine
+         (with-color (rgba "black")
+           (with-emitted (emitted rgb (* int 32))
+             (sphere pos #i1/16)))
+         (light pos (emitted rgb int))))))))
+
+(define frozen-spheres (time (freeze spheres)))
+
+(define frame (new frame% [label "Sphere Field"] [width 800] [height 600]))
+(define canvas (new pict3d-canvas% [parent frame]))
+(send frame show #t)
+
+(require profile)
+
+(profile
+ (let loop ([i 0])
+   (when (send frame is-shown?)
+     (time
+      (define start-time (fl (current-inexact-milliseconds)))
+      (define cx (cos (degrees->radians i)))
+      (define sx (sin (degrees->radians i)))
+      (define cx2 (cos (* 2 (degrees->radians i))))
+      (define sx2 (sin (* 2 (degrees->radians i))))
+      
+      (define camera
+        (point-at (pos (* -6 cx) (* -6 sx) (* 0.5 sx)) origin))
+      
+      (define light-pos
+        (pos (* 2 cx2) (* 2 sx2) (* 2 sx2)))
+      
+      (define pict
+        (combine
+         (basis 'camera camera)
+         frozen-spheres
+         (with-color (rgba "black")
+           (with-emitted (emitted 1 1 1 4)
+             (sphere light-pos 0.5)))
+         (light light-pos (emitted "silver" 20))))
+      
+      (send canvas set-pict3d pict)
+      ;; Rate-limit to 60 FPS
+      (define end-time (fl (current-inexact-milliseconds)))
+      (define delay (* #i1/1000 (max 1.0 (- #i1000/60 (- end-time start-time)))))
+      (sleep/yield delay)
+      )
+     (loop (+ i 1)))))
+
