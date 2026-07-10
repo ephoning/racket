b8fd7b35490799597355d5df45275913c8980d56
E153245@constellation.com <Edgar.Honing@constellation.com>
2026-07-10 11:37:15 -0700

    minor update

--- graphics-experiment.rkt
+++ graphics-experiment.rkt
@@ -9,11 +9,35 @@
 ; viewport window appears
  
 ((draw-line w) (make-posn 30 30) (make-posn 100 100))
+((draw-line w) (make-posn 40 40) (make-posn 80 100))
+
+(sleep 1)
+
+((clear-viewport w))
+
+((draw-line w) (make-posn 30 30) (make-posn 100 110))
+((draw-line w) (make-posn 40 40) (make-posn 80 110))
+
+
 ; line appears
  
 ;(close-viewport w)
 ; viewport disappears
  
 ;(close-graphics)
+; again, nothing appears to happen, but
+; unclosed viewports (if any) would disappear
+
+
+(for ([i
+       (range 0 200 10)])
+  ((draw-line w) (make-posn 30 30) (make-posn (+ i 10) (+ i 100)))
+  (sleep 1)
+  )
+
+(close-viewport w)
+; viewport disappears
+ 
+(close-graphics)
 ; again, nothing appears to happen, but
 ; unclosed viewports (if any) would disappear
