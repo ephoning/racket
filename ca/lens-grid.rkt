6671040a92b6eac2fa9028a77ce05a9f2ca4cdd1
e153245@constellation.com <Edgar.Honing@constellation.com>
2026-08-27 22:13:20 -0700

    more bulk in fill-grid; not complete yet

--- ca/lens-grid.rkt
+++ ca/lens-grid.rkt
@@ -10,6 +10,11 @@
 (require lens/common)
 (require lens/data/hash)
 
+;; generate random true/false
+;;  perc [0-100] = precentage true
+(define (random-t-f perc)
+  (let ([r (random 0 100)])
+    (<= r perc)))
 
 (define (ca-create-lens-grid xsize ysize)
   (hash 'general (hash 'xsize xsize 'ysize ysize) 'grid (hash)))
@@ -18,6 +23,9 @@
   (define ca-grid-xy-lens (hash-ref-nested-lens 'grid `(,x ,y)))
   (lens-set ca-grid-xy-lens ca-grid payload))
 
+(define ca-xsize-lens (hash-ref-nested-lens 'general 'xsize))
+(define ca-ysize-lens (hash-ref-nested-lens 'general 'ysize))
+
 ;; TESTING
 
 (define my-grid (ca-create-lens-grid 10 10))
@@ -25,7 +33,10 @@
 
 ;; TODO: file the 10x10 grid with cells randomly alive with probability p
 (define (fill-grid lens-grid alive-prob)
-  'TODO)
+  (let* ([xsize (lens-view ca-xsize-lens lens-grid)]
+         [ysize (lens-view ca-ysize-lens lens-grid)]
+         [xy-pairs (cartesian-product (stream->list (in-range 0 xsize)) (stream->list (in-range 0 ysize)))])
+    (printf "~a / ~a / ~a" xsize ysize xy-pairs)))
 
 #|
 ;; immutable hash table; suitable for use in lenses
@@ -46,3 +57,10 @@
 
 (display (lens-view (hash-ref-nested-lens 'sub 'foobar) d__))
 |#
+
+(cartesian-product (stream->list (in-range 0 2)) (stream->list (in-range 0 3)))
+
+(for ([x (in-range 0 2)])
+  (for ([y (in-range 0 3)])
+    (printf "~a / ~a\n" x y)))
+
