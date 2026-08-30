f5a19a49234a5dd151fe065e555a1f95160f68eb
e153245@constellation.com <Edgar.Honing@constellation.com>
2026-08-30 10:18:50 -0700

    structure CA life per MVC

---
+++ ca/control.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+;; main file for ca life experiment
+;;
+;; dependencies
+;; - model: lens-grid based state representation
+;; - view: graphics for 2d visualixation
+;; - this file: control - driver/main loop
+
+(require "model.rkt" "view.rkt")
+
+(define my-grid (ca-create-lens-grid 5 5))
+
+(fill-grid my-grid 42)
