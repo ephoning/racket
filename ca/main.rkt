03cdd54771e105383d5f2327208de58ab7af33bf
e153245@constellation.com <Edgar.Honing@constellation.com>
2026-08-30 10:47:40 -0700

    re-intro main

--- ca/main.rkt
+++ ca/main.rkt
@@ -3,11 +3,11 @@
 ;; main file for ca life experiment
 ;;
 ;; dependencies
-;; - lens-grid: model - state representation
-;; - display:   view - graphics for 2d visualixation
-;; - this file: control - driver/main loop
+;; - model: lens-grid based state representation
+;; - view: graphics for 2d visualixation
+;; - control: - driver/main loop
 
-(require "lens-grid.rkt")
+(require "model.rkt" "view.rkt", "control.rkt")
 
 (define my-grid (ca-create-lens-grid 5 5))
 
